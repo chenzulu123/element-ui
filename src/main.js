5f2087a1e50c6e5fd563ce1fdc7cac1563c98996
陈祖路 <chenzulu@163.com>
2018-10-22 21:46:47 +0800

    --

--- src/main.js
+++ src/main.js
@@ -8,20 +8,8 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/font-icon/iconfont.css'
 Vue.config.productionTip = false
-// 使用element-ui
 Vue.use(ElementUI);
-// import VueI18n from 'vue-i18n'
 import i18n from './common/lang/i18n'
-// Vue.use(VueI18n) // 通过插件的形式挂载
-// import cn from './common/lang/zh';
-// import en from './common/lang/en';
-// const i18n = new VueI18n({
-//     messages: {
-//       'zh-CN': cn,   // 中文语言包
-//       'en-US': en    // 英文语言包
-//     }
-// })
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
